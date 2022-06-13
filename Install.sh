544efdf469951386735aaa3626578075f89773eb
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-06-13 20:03:56 +0800

    细节优化

--- Install.sh
+++ Install.sh
@@ -13,7 +13,7 @@ command -v pkg &>/dev/null&&echo "
 $Y- 正在安装依赖$O
 "||abort "找不到pkg命令，请确认安装了正确的Termux环境"
 #sed -i 's@^\(deb.*stable main\)$@#\1\ndeb https://mirrors.bfsu.edu.cn/termux/apt/termux-main stable main@' "$PREFIX/etc/apt/sources.list"
-pkg up -y&&pkg i -y curl nano tmux tsu whiptail||abort "依赖安装失败"
+pkg up -y&&pkg i -y curl nano perl tmux tsu whiptail||abort "依赖安装失败"
 abort_update(){ echo "
 $R! $@$O";[ "$N" -lt "8" ]&&{ let N++;download;}||abort "脚本下载失败，请检查网络，并尝试重新下载";}
 download(){ case "$N" in
