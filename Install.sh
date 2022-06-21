b8688de8300047e7d3ed16c86d5991d89808904c
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-06-22 00:04:58 +0800

    细节优化

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 #TRSS_OneBot 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202206160
+NAME=v1.0.0;VERSION=202206210
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y OneBot$G Install$C Script$O
@@ -26,10 +26,10 @@ download(){ case "$N" in
   "7")SERVER="Bitbucket";URL="https://bitbucket.org/TimeRainStarSky/TRSS_OneBot/raw/main";;
   "8")SERVER="Jsdelivr";URL="https://cdn.jsdelivr.net/gh/TimeRainStarSky/TRSS_OneBot@main"
   esac;echo "
-  正在从 $SERVER 服务器 下载版本信息";GETVER="$(curl "$URL/version")"||abort_update "下载失败";NEWVER="$(echo -n "$GETVER"|sed -n s/^version=//p)";NEWNAME="$(echo -n "$GETVER"|sed -n s/^name=//p)";MD5="$(echo -n "$GETVER"|sed -n s/^md5=//p)";[ -n "$NEWVER" ]&&[ -n "$NEWNAME" ]&&[ -n "$MD5" ]||abort_update "下载文件版本信息缺失";echo "
+  正在从 $SERVER 服务器 下载版本信息";GETVER="$(geturl "$URL/version")"||abort_update "下载失败";NEWVER="$(echo -n "$GETVER"|sed -n s/^version=//p)";NEWNAME="$(echo -n "$GETVER"|sed -n s/^name=//p)";MD5="$(echo -n "$GETVER"|sed -n s/^md5=//p)";[ -n "$NEWVER" ]&&[ -n "$NEWNAME" ]&&[ -n "$MD5" ]||abort_update "下载文件版本信息缺失";echo "
 $B  最新版本：$G$NEWNAME$C ($NEWVER)$O
 
-  开始下载";mkdir -p "$DIR";curl "$URL/Main.sh">"$DIR/Main.sh"||abort_update "下载失败";[ "$(md5sum "$DIR/Main.sh"|head -c 32)" != "$MD5" ]&&abort_update "下载文件校验错误";echo -n "bash '$DIR/Main.sh'">"$PREFIX/bin/tsob"||abort "脚本执行命令$PREFIX/bin/tsob设置失败";chmod 755 "$PREFIX/bin/tsob"||abort "脚本权限设置失败";echo "
+  开始下载";mkdir -p "$DIR";geturl "$URL/Main.sh">"$DIR/Main.sh"||abort_update "下载失败";[ "$(md5sum "$DIR/Main.sh"|head -c 32)" != "$MD5" ]&&abort_update "下载文件校验错误";echo -n "bash '$DIR/Main.sh'">"$PREFIX/bin/tsob"||abort "脚本执行命令$PREFIX/bin/tsob设置失败";chmod 755 "$PREFIX/bin/tsob"||abort "脚本权限设置失败";echo "
 $G- 脚本安装完成，输入tsob执行$O";rm -rf "$0";exit;}
 echo "
-$Y- 正在下载脚本$O";N=1;download
+$Y- 正在下载脚本$O";alias geturl="curl -L --retry 2 --connect-timeout 5";N=1;download
