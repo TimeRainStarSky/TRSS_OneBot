714c45449c4ef4310682b2d96d70a3a0aa3e317d
🌌 <Time.Rain.Star.Sky@Gmail.com>
2022-10-08 12:13:38 +0800

    优化 安装脚本

--- Install.sh
+++ Install.sh
@@ -1,5 +1,5 @@
 #TRSS OneBot 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202210040
+NAME=v1.0.0;VERSION=202210080
 R="[1;31m";G="[1;32m";Y="[1;33m";C="[1;36m";B="[1;m";O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y OneBot$G Install$C Script$O
@@ -9,6 +9,8 @@ $B——————————————————————————
 abort(){ echo "
 $R! $@$O";exit 1;}
 DIR="$HOME/TRSS_OneBot"
+CMD="${CMD:-tsob}"
+CMDPATH="${CMDPATH:-${PREFIX:-/usr/local}/bin}"
 if type curl dialog micro neofetch perl ranger tmux &>/dev/null;then
   echo "
 $G- 依赖已安装$O"
@@ -56,8 +58,8 @@ $B  最新版本：$G$NEWNAME$C ($NEWVER)$O
 mkdir -vp "$DIR"
 geturl "$URL/Main.sh">"$DIR/Main.sh"||abort_update "下载失败"
 [ "$(md5sum "$DIR/Main.sh"|head -c 32)" != "$MD5" ]&&abort_update "下载文件校验错误"
-echo -n "bash '$DIR/Main.sh' "'"$@"'>"${PREFIX:-/usr/local}/bin/tsob"&&chmod 755 "${PREFIX:-/usr/local}/bin/tsob"||abort "脚本执行命令 ${PREFIX:-/usr/local}/bin/tsob 设置失败，手动执行命令：bash '$DIR/Main.sh'"
+echo -n "bash '$DIR/Main.sh' "'"$@"'>"$CMDPATH/$CMD"&&chmod 755 "$CMDPATH/$CMD"||abort "脚本执行命令 $CMDPATH/$CMD 设置失败，手动执行命令：bash '$DIR/Main.sh'"
 echo "
-$G- 脚本安装完成，输入tsob执行$O";exit;}
+$G- 脚本安装完成，输入 $CMD 执行$O";exit;}
 echo "
 $Y- 正在下载脚本$O";geturl(){ curl -L --retry 2 --connect-timeout 5 "$@";};N=1;download
