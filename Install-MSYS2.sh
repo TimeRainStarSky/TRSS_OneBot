fd200711df38ba54c6d6f34063d440766cda92bc
🌌 <Time.Rain.Star.Sky@Gmail.com>
2023-01-15 15:00:48 +0800

    优化 MSYS2 安装脚本

--- Install-MSYS2.sh
+++ Install-MSYS2.sh
@@ -1,5 +1,5 @@
 #TRSS OneBot MSYS2 安装脚本 作者：时雨🌌星空
-NAME=v1.0.0;VERSION=202301080
+NAME=v1.0.0;VERSION=202301150
 R="[1;31m" G="[1;32m" Y="[1;33m" C="[1;36m" B="[1;m" O="[m"
 echo "$B————————————————————————————
 $R TRSS$Y OneBot$G Install$C Script$O
@@ -17,13 +17,14 @@ MSYS2ENV=mingw-w64-ucrt-x86_64
 type pacman &>/dev/null&&echo "
 $Y- 正在安装依赖$O
 "||abort "找不到 pacman 命令，请确认安装了正确的 MSYS2 环境"
-pacman -Syu --noconfirm --needed --overwrite "*" curl dialog git tmux tmate perl neofetch unzip fish ncdu $MSYS2ENV-ripgrep $MSYS2ENV-fd $MSYS2ENV-fzf $MSYS2ENV-bat ruby||abort "依赖安装失败"
+pacman -Syu --noconfirm --needed --overwrite "*" curl dialog tmux tmate perl neofetch unzip fish ncdu $MSYS2ENV-ripgrep $MSYS2ENV-fd $MSYS2ENV-fzf $MSYS2ENV-bat ruby||abort "依赖安装失败"
 type fastfetch &>/dev/null||ln -vsf neofetch "$(dirname "$(command -v neofetch)")/fastfetch"
 [ -s /usr/bin/rg ]||echo -n 'exec /ucrt64/bin/rg "$@"'>/usr/bin/rg
 [ -s /ucrt64/bin/fd ]&&mv -vf /ucrt64/bin/fd /ucrt64/bin/fdo
 [ -s /usr/bin/fd ]||echo -n '/ucrt64/bin/fdo "$@"|cygpath -mf -'>/usr/bin/fd
 [ -s /usr/bin/fzf ]||echo -n 'exec /ucrt64/bin/fzf "$@"'>/usr/bin/fzf
-[ -s /usr/bin/bat ]||echo -n 'exec /ucrt64/bin/bat "$@"'>/usr/bin/bat
+[ -s /ucrt64/bin/bat ]&&mv -vf /ucrt64/bin/bat /ucrt64/bin/bato
+[ -s /usr/bin/bat ]||echo -n 'USERPROFILE="$HOMEDRIVE$HOMEPATH" exec /ucrt64/bin/bato "$@"'>/usr/bin/bat
 [ -s /usr/bin/lolcat ]||{ gem install lolcat&&echo exec "$HOME/.local/share/gem/ruby/"*/bin/lolcat '"$@"'>/usr/bin/lolcat;}
 
 Title="TRSS OneBot Install Script $NAME ($VERSION)";BackTitle="作者：时雨🌌星空"
